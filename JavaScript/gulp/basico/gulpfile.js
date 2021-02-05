4a900c63aef70f625a59c727db1d7855d086474e
ManoMax <gabrielmaxcontato@gmail.com>
2021-02-05 15:55:44 -0300

    feat: adding new files in paste with gulp

--- JavaScript/gulp/basico/gulpfile.js
+++ JavaScript/gulp/basico/gulpfile.js
@@ -1,11 +1,35 @@
 const gulp = require('gulp')
 
-// const { series } = require('gulp')
-const series = gulp.series
+const { series, parallel } = require('gulp')
+// const series = gulp.series
 
-function copiar(callback) {
-    console.log('Tarefa de copiar!')
+const antes1 = callback => {
+    console.log('Tarefa antes 1!')
     return callback()
 }
 
-module.exports.default = series(copiar)
+const antes2 = callback => {
+    console.log('Tarefa antes 2!')
+    return callback()
+}
+
+function copiar(callback) {
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+
+    gulp.src('pastaA/**/*.txt')
+    // pipe = aplica transformações nos arquivos que selecionamos
+        .pipe(gulp.dest('pastaB'))
+    // dest = coloca os arquivos em uma pasta de destino.
+    return callback()
+}
+
+const fim = callback => {
+    console.log('Tarefa Fim!')
+    return callback()
+}
+
+module.exports.default = series(
+    parallel( antes1, antes2),
+    copiar,
+    fim
+)
