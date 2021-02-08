c2be038372dc41e4145e5304c86b38083cf7d5c3
ManoMax <gabrielmaxcontato@gmail.com>
2021-02-08 16:07:41 -0300

    docs: adding comment about the destination of the file

--- JavaScript/webpack/src/pessoa.js
+++ JavaScript/webpack/src/pessoa.js
@@ -2,6 +2,8 @@
 // import './modulos/moduloA'
 // O comando 'npm start ; node dist/main.js' irá encontrar o Module B
 
+// O comando foi atualizado, manualmente, para 'npm start ; node public/principal.js'
+
 export default class Pessoa {
     cumprimentar() {
         return 'Bom dia!'
