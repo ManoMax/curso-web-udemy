fdf30165caffcda1f9eb0dee0e7254f1aa758c08
ManoMax <gabrielmaxcontato@gmail.com>
2021-02-08 17:04:58 -0300

    feat: load CSS files with webpack

--- JavaScript/webpack/webpack.config.js
+++ JavaScript/webpack/webpack.config.js
@@ -7,5 +7,14 @@ module.exports = {
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader' // interpreta @import, url()...
+            ]
+        }]
     }
 }
