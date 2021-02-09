9bf7acac778dc9d5dccdd7bfa748f4b1c2e97fbf
ManoMax <gabrielmaxcontato@gmail.com>
2021-02-09 08:25:21 -0300

    feat: script for dev and production environment

--- JavaScript/webpack/webpack.config.js
+++ JavaScript/webpack/webpack.config.js
@@ -1,13 +1,27 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
 
 module.exports = {
     // mode: 'production',
-    mode: 'development',
+    // mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
