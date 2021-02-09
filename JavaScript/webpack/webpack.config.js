e21e60738ba648025937cd6f032869215859b3e8
ManoMax <gabrielmaxcontato@gmail.com>
2021-02-09 10:06:24 -0300

    feat: config webserver for dev

--- JavaScript/webpack/webpack.config.js
+++ JavaScript/webpack/webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
     optimization: {
         minimizer: [
             new OptimizeCSSAssetsPlugin({})
