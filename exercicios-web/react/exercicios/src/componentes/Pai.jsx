f1d4d4e39cd33242dc9fe8db357cf1569b9966a8
ManoMax <gabrielmaxcontato@gmail.com>
2021-02-17 16:16:41 -0300

    feat: another alternative for inheritance between parent and child components

--- exercicios-web/react/exercicios/src/componentes/Pai.jsx
+++ exercicios-web/react/exercicios/src/componentes/Pai.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import Filho from './Filho'
+
+import { childrenWithProps } from '../utils/utils'
 
 export default props =>
     <div>
         <h1>{props.nome} {props.sobrenome}</h1>
         <h2>Filhos</h2>
         <ul>
-            <Filho nome="Pedro" sobrenome={props.sobrenome} />
-            <Filho {...props} />
-            <Filho {...props} nome="Carla" />
-            {/* No caso abaixo, props sobrescreve "Maria" */}
-            <Filho nome="Maria" {...props} />
+            {/* {props.children} */}
+            {childrenWithProps(props)}
         </ul>
     </div>
--- exercicios-web/react/exercicios/src/index.js
+++ exercicios-web/react/exercicios/src/index.js
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>
-        <Pai nome="Paulo" sobrenome="Silva"/>
+        <Pai nome="Paulo" sobrenome="Silva">
+            {/* Como passo os componentes Filhos aqui? */}
+            <Filho nome="Pedro" />
+            <Filho nome="Paulo" />
+            <Filho nome="Carla" />
+        </Pai>
     </div>    
 , document.getElementById('root'))
