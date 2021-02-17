e8a12fd40708ce5f503427a982de0e3f1040b8d8
ManoMax <gabrielmaxcontato@gmail.com>
2021-02-17 08:43:54 -0300

    feat: multiple components in one file

--- exercicios-web/react/exercicios/src/index.js
+++ exercicios-web/react/exercicios/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import BomDia from './componentes/BomDia'
+import BoaTarde, { BoaNoite } from './componentes/Multiplos'
 
-ReactDOM.render(<BomDia nome="Guilherme" idade={10}/>, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <BoaTarde nome="Ana" />
+        <BoaNoite nome="Bia" />
+    </div>    
+, document.getElementById('root'))
