b44bc19cf3c41b5dbb27bbf9a5269348efd2c860
ManoMax <gabrielmaxcontato@gmail.com>
2021-02-17 10:23:54 -0300

    feat: setState with constructor function of Component

--- exercicios-web/react/exercicios/src/componentes/Saudacao.jsx
+++ exercicios-web/react/exercicios/src/componentes/Saudacao.jsx
@@ -7,16 +7,14 @@ export default class Saudacao extends Component {
         nome: this.props.nome
     }
 
+    constructor(props) {
+        super(props)
+
+        this.setTipo = this.setTipo.bind(this)
+        this.setNome = this.setNome.bind(this)
+    }
+
     setTipo(e) {
-        // console.log(e.target.value)
-
-        // Isso abaixo, não funciona:
-        // this.props.tipo = e.target.value
-
-        // let i = 1
-        // setInterval(() => {
-        //     this.setState({ tipo: ++i })
-        // }, 1000)
         this.setState({ tipo: e.target.value })
     }
 
@@ -25,16 +23,15 @@ export default class Saudacao extends Component {
     }
 
     render() {
-        // const { tipo, nome } = this.props
         const { tipo, nome } = this.state
         return (
             <div>
                 <h1>{tipo} {nome}!</h1>
                 <hr />
                 <input type="text" placeholder="Tipo..."
-                    value={tipo} onChange={e => this.setTipo(e)}/>
+                    value={tipo} onChange={this.setTipo} />
                 <input type="text" placeholder="Nome..."
-                    value={nome} onChange={e => this.setNome(e)}/>
+                    value={nome} onChange={e => this.setNome(e)} />
             </div>
         )
     }
