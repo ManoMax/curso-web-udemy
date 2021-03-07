e151bc84ff9eb3657b2b416cf8a2c56bf60c234b
ManoMax <gabrielmaxcontato@gmail.com>
2021-03-07 18:15:47 -0300

    feat: Implementing Routes (React Router)

--- exercicios-web/react/crud/frontend/src/main/App.jsx
+++ exercicios-web/react/crud/frontend/src/main/App.jsx
@@ -2,16 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Home from '../components/home/Home'
+import Routes from './Routes'
 import Footer from '../components/template/Footer'
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Home />
-        <Footer />
-    </div>
+// No lugar de BrowserRouter, pode-se usar o HashRouter (Hash fica com /#/)
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
